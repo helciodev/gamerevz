a53aadf21ae0a7760bc030af5672d4b3a5483f41
helcio <h.andre@giap.pl>
2024-08-12 13:01:23 +0100

    Add initial image assets for seed games

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root "game#index"
+root "games#index"
 
   resources :games
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ makefile
@@ -0,0 +1,2 @@
+make start:
+	rails server
