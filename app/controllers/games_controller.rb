aa891fc74a6a5b381b877673f26bae554387ce99
helcio <h.andre@giap.pl>
2024-08-14 11:28:07 +0100

    Add new and create game controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,11 +9,17 @@ before_action :get_game, except:[:new, :create, :index]
   end
 
   def new
-
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
 
+    if @game.save
+      redirect_to @game, notice:"#{@game.title} created successfuly"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -21,8 +27,14 @@ before_action :get_game, except:[:new, :create, :index]
   end
 
   def update
+    if @game.update(game_params)
+      redirect_to @game, notice:"#{@game.title} updated sucessfuly"
+    else
 
+      render :edit, status: :unprocessable_entity
+    end
   end
+
 
   def destroy
 
@@ -32,7 +44,7 @@ before_action :get_game, except:[:new, :create, :index]
   private
 
   def game_params
-
+    params.require(:game).permit(:title, :directors, :writers, :description, :cover_image, :release_date, :production_company)
   end
 
   def get_game
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,9 @@ class Game < ApplicationRecord
   before_save :get_slug
 
   validates :title, presence: true, uniqueness: true
+  validates :production_company, presence: true
+  validates :description, length: {minimum:90, maximum:300}
+  validates :cover_image, format: {with: /\w+\.(jpeg|png)\z/, message:'Image has to be jpeg or png file'}
 
   def to_param
     slug
